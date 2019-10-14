2357d016af857a5b9f37ba5f00629c1ae1a9b4cf
Jacky Feng <jackyfeng530@yahoo.com>
2019-10-14 17:30:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,10 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	 @post = Post.new(post_params)
+  @post = Post.new(post_params(:title, :description))
   @post.save
   redirect_to post_path(@post)
-	end
+end
 
 	def update
 	  @post = Post.find(params[:id])
